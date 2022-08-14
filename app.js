6ba2e8527fafb973b43ea2e3b06aff2755b067d7
Nguyễn Xuân Bách <110024359+Yukimarin@users.noreply.github.com>
2022-08-15 00:52:46 +0700

    CSS

---
+++ app.js
@@ -0,0 +1,20 @@
+// //Thứ tự ưu tiên 
+// inline (viết trực tiếp trong html) - internal (viết trong thẻ style) - external (viết trong file css)
+// !important (độ ưu tiên cao hơn ca inline)
+
+// //Css selectors
+// id,class (id có thứ tự ưu tiên lớn hơn thằng class)
+// C1: Sử dụng dấu # tên id
+// #demo-id{
+//     color: red;
+// }
+// C2: Sử dụng class
+
+// //Css specificity
+
+// Box model 
+// margin, padding, border, width, height
+// Box-model là gì
+
+// Thẻ inline (span)
+// không nhận width và height 
